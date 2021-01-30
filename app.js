d529f8577952cf39b4f967363a1cfe22c95195f7
hhhhhhdong <yhd520@naver.com>
2021-01-31 01:20:54 +0900

    init

---
+++ app.js
@@ -0,0 +1,155 @@
+const wrap = document.querySelector("#wrap");
+const setting = wrap.querySelector("#setting");
+const [white, black] = wrap.querySelectorAll(".timer");
+const whiteSpan = white.querySelector("span");
+const blackSpan = black.querySelector("span");
+const timeDiv = setting.querySelector(".time");
+const plusDiv = setting.querySelector(".plus");
+const minusDiv = setting.querySelector(".minus");
+const pauseDiv = setting.querySelector(".pause");
+const pauseIcon = pauseDiv.querySelector("i");
+const resetDiv = setting.querySelector(".reset");
+
+let isStarted = false;
+let blackTurn = false;
+let whiteTurn = false;
+let initTime;
+let blackTime;
+let whiteTime;
+let blackInterval;
+let whiteInterval;
+let isPaused = false;
+let nowTurn;
+
+const showTime = (span, time) => {
+  let min = Math.floor(Math.abs(time) / 60);
+  let sec = Math.abs(time) % 60;
+  console.log(sec);
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+
+  if (time < 0) {
+    span.innerHTML = `- ${min} : ${sec}`;
+  } else {
+    span.innerHTML = `${min} : ${sec}`;
+  }
+};
+
+const onClickWhite = (e) => {
+  if (isPaused) return;
+  if (blackTurn) return;
+  if (!isStarted) {
+    timeDiv.classList.remove("whiteColor");
+    timeDiv.classList.add("gray");
+    pauseDiv.classList.remove("gray");
+    pauseDiv.classList.add("whiteColor");
+    blackTime = initTime;
+    whiteTime = initTime;
+    isStarted = true;
+  }
+  whiteTurn = false;
+  blackTurn = true;
+  whiteInterval && clearInterval(whiteInterval);
+  blackInterval = setInterval(() => {
+    blackTime--;
+    showTime(blackSpan, blackTime);
+  }, 1000);
+};
+
+const onClickBlack = (e) => {
+  if (isPaused) return;
+  if (whiteTurn) return;
+  if (!isStarted) {
+    timeDiv.classList.remove("whiteColor");
+    timeDiv.classList.add("gray");
+    pauseDiv.classList.remove("gray");
+    pauseDiv.classList.add("whiteColor");
+    whiteTime = initTime;
+    blackTime = initTime;
+    isStarted = true;
+  }
+  whiteTurn = true;
+  blackTurn = false;
+  blackInterval && clearInterval(blackInterval);
+  whiteInterval = setInterval(() => {
+    whiteTime--;
+    showTime(whiteSpan, whiteTime);
+  }, 1000);
+};
+
+const onClickPlus = () => {
+  if (isStarted) return;
+  initTime += 60;
+  showTime(whiteSpan, initTime);
+  showTime(blackSpan, initTime);
+};
+
+const onClickMinus = () => {
+  if (isStarted) return;
+  if (initTime <= 60) return;
+  initTime -= 60;
+  showTime(whiteSpan, initTime);
+  showTime(blackSpan, initTime);
+};
+
+const onClickPause = () => {
+  if (!isStarted) return;
+  if (!isPaused) {
+    isPaused = true;
+    if (whiteTurn) {
+      nowTurn = "white";
+      clearInterval(whiteInterval);
+      clearInterval(blackInterval);
+    } else if (blackTurn) {
+      nowTurn = "black";
+      clearInterval(blackInterval);
+      clearInterval(whiteInterval);
+    }
+    pauseIcon.className = "fas fa-play center";
+  } else {
+    isPaused = false;
+    if (nowTurn === "white") {
+      whiteInterval = setInterval(() => {
+        whiteTime--;
+        showTime(whiteSpan, whiteTime);
+      }, 1000);
+    } else {
+      blackInterval = setInterval(() => {
+        blackTime--;
+        showTime(blackSpan, blackTime);
+      }, 1000);
+    }
+    pauseIcon.className = "fas fa-pause center";
+  }
+};
+
+const onClickReset = () => {
+  timeDiv.className = "time whiteColor";
+  pauseDiv.className = "pause gray";
+  pauseIcon.className = "fas fa-pause center";
+  nowTurn = "";
+  isStarted = false;
+  isPaused = false;
+  whiteTurn = false;
+  blackTurn = false;
+  initTime = 600;
+  whiteInterval && clearInterval(whiteInterval);
+  blackInterval && clearInterval(blackInterval);
+  showTime(whiteSpan, initTime);
+  showTime(blackSpan, initTime);
+};
+
+function init() {
+  black.addEventListener("click", onClickBlack);
+  white.addEventListener("click", onClickWhite);
+  plusDiv.addEventListener("click", onClickPlus);
+  minusDiv.addEventListener("click", onClickMinus);
+  pauseDiv.addEventListener("click", onClickPause);
+  resetDiv.addEventListener("click", onClickReset);
+  onClickReset();
+}
+init();
